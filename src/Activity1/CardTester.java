04dfb3ca5be172c314b3ad0fd3342f35bc7e6159
BT_1N3_30 <cpolisi6526@bths.edu>
2019-02-13 08:37:24 -0500

    card class done

--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,13 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    static Card one= new Card("2","heart",2);
+    static Card two= new Card("3", "diamonds",3);
+    static Card three= new Card("2","heart",2);
+
+    public static void main(String[] args){
+        one.toString();
+        one.matches(two);
+        one.matches(three);
+    }
 }
